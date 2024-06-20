a5a2a6c4785f27bb8074b4d08aa66cb8857572fc
MaketendoDev <najim.oria59@hotmail.com>
2024-06-20 21:59:51 +0100

    Removed Unused Models + Added NEW Exterior Textures (not implemented yet ): )

--- src/main/java/net/maketendo/tardifmod/init/TardifModModModels.java
+++ src/main/java/net/maketendo/tardifmod/init/TardifModModModels.java
@@ -9,34 +9,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.api.distmarker.Dist;
 
-import net.maketendo.tardifmod.client.model.Modelweeping_angel_two;
-import net.maketendo.tardifmod.client.model.Modelweeping_angel_three;
-import net.maketendo.tardifmod.client.model.Modelweeping_angel_one;
-import net.maketendo.tardifmod.client.model.Modeltenth_cyberman;
-import net.maketendo.tardifmod.client.model.Modelpolicebox_fourteen;
 import net.maketendo.tardifmod.client.model.Modelmondasian_cybervillager;
 import net.maketendo.tardifmod.client.model.Modellaser;
-import net.maketendo.tardifmod.client.model.Modelhartnell_console;
-import net.maketendo.tardifmod.client.model.Modelcybermantenth;
-import net.maketendo.tardifmod.client.model.Modelcopper_console;
-import net.maketendo.tardifmod.client.model.Modelclassix_box;
-import net.maketendo.tardifmod.client.model.ModelCustomModel;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class TardifModModModels {
 	@SubscribeEvent
 	public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
 		event.registerLayerDefinition(Modellaser.LAYER_LOCATION, Modellaser::createBodyLayer);
-		event.registerLayerDefinition(Modelweeping_angel_two.LAYER_LOCATION, Modelweeping_angel_two::createBodyLayer);
-		event.registerLayerDefinition(Modelclassix_box.LAYER_LOCATION, Modelclassix_box::createBodyLayer);
 		event.registerLayerDefinition(Modelmondasian_cybervillager.LAYER_LOCATION, Modelmondasian_cybervillager::createBodyLayer);
-		event.registerLayerDefinition(Modelpolicebox_fourteen.LAYER_LOCATION, Modelpolicebox_fourteen::createBodyLayer);
-		event.registerLayerDefinition(Modelweeping_angel_one.LAYER_LOCATION, Modelweeping_angel_one::createBodyLayer);
-		event.registerLayerDefinition(Modelcopper_console.LAYER_LOCATION, Modelcopper_console::createBodyLayer);
-		event.registerLayerDefinition(Modelcybermantenth.LAYER_LOCATION, Modelcybermantenth::createBodyLayer);
-		event.registerLayerDefinition(Modeltenth_cyberman.LAYER_LOCATION, Modeltenth_cyberman::createBodyLayer);
-		event.registerLayerDefinition(Modelweeping_angel_three.LAYER_LOCATION, Modelweeping_angel_three::createBodyLayer);
-		event.registerLayerDefinition(ModelCustomModel.LAYER_LOCATION, ModelCustomModel::createBodyLayer);
-		event.registerLayerDefinition(Modelhartnell_console.LAYER_LOCATION, Modelhartnell_console::createBodyLayer);
 	}
 }
--- src/main/java/net/maketendo/tardifmod/init/TardifModModSounds.java
+++ src/main/java/net/maketendo/tardifmod/init/TardifModModSounds.java
@@ -34,4 +34,5 @@ public class TardifModModSounds {
 	public static final RegistryObject<SoundEvent> SONIC_SCREWDRIVER = REGISTRY.register("sonic_screwdriver", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("tardif_mod", "sonic_screwdriver")));
 	public static final RegistryObject<SoundEvent> SKARO_AMBIENCE = REGISTRY.register("skaro_ambience", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("tardif_mod", "skaro_ambience")));
 	public static final RegistryObject<SoundEvent> TENTH_MESSAGE = REGISTRY.register("tenth_message", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("tardif_mod", "tenth_message")));
+	public static final RegistryObject<SoundEvent> TARDIS_SOUND_MUSIC_FOREVER = REGISTRY.register("tardis_sound_music_forever", () -> SoundEvent.createVariableRangeEvent(new ResourceLocation("tardif_mod", "tardis_sound_music_forever")));
 }
