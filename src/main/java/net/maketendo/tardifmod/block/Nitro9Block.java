cd25f1f7e7c8f397618f052f3df5321ccc5f40a7
MaketendoDev <najim.oria59@hotmail.com>
2024-06-22 09:38:50 +0100

    Updated the texture for some blocks now need to remove old stuff

--- src/main/java/net/maketendo/tardifmod/block/Nitro9Block.java
+++ src/main/java/net/maketendo/tardifmod/block/Nitro9Block.java
@@ -5,13 +5,19 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.FallingBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
@@ -21,8 +27,11 @@ import net.minecraft.core.BlockPos;
 import net.maketendo.tardifmod.procedures.Nitro9OnBlockRightClickedProcedure;
 
 public class Nitro9Block extends FallingBlock {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
 	public Nitro9Block() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(1f, 10f).requiresCorrectToolForDrops().noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
@@ -42,7 +51,30 @@ public class Nitro9Block extends FallingBlock {
 
 	@Override
 	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-		return Shapes.or(box(5.5, 0, 5.5, 10.5, 8, 10.5), box(6.5, 8, 6.5, 9.5, 10, 9.5));
+		return switch (state.getValue(FACING)) {
+			default -> Shapes.or(box(5.5, 0, 5.5, 10.5, 8, 10.5), box(6.5, 8, 6.5, 9.5, 10, 9.5));
+			case NORTH -> Shapes.or(box(5.5, 0, 5.5, 10.5, 8, 10.5), box(6.5, 8, 6.5, 9.5, 10, 9.5));
+			case EAST -> Shapes.or(box(5.5, 0, 5.5, 10.5, 8, 10.5), box(6.5, 8, 6.5, 9.5, 10, 9.5));
+			case WEST -> Shapes.or(box(5.5, 0, 5.5, 10.5, 8, 10.5), box(6.5, 8, 6.5, 9.5, 10, 9.5));
+		};
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
 
 	@Override
--- src/main/java/net/maketendo/tardifmod/block/TheMomentBlock.java
+++ src/main/java/net/maketendo/tardifmod/block/TheMomentBlock.java
@@ -5,12 +5,18 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
@@ -20,8 +26,11 @@ import net.minecraft.core.BlockPos;
 import net.maketendo.tardifmod.procedures.TheMomentOnBlockRightClickedProcedure;
 
 public class TheMomentBlock extends Block {
+	public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
+
 	public TheMomentBlock() {
 		super(BlockBehaviour.Properties.of().sound(SoundType.METAL).strength(1f, 10f).noOcclusion().isRedstoneConductor((bs, br, bp) -> false));
+		this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 	}
 
 	@Override
@@ -42,6 +51,24 @@ public class TheMomentBlock extends Block {
 	@Override
 	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
 		return Shapes.empty();
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+	}
+
+	public BlockState rotate(BlockState state, Rotation rot) {
+		return state.setValue(FACING, rot.rotate(state.getValue(FACING)));
+	}
+
+	public BlockState mirror(BlockState state, Mirror mirrorIn) {
+		return state.rotate(mirrorIn.getRotation(state.getValue(FACING)));
 	}
 
 	@Override
