f867e7389ed54bad5ee88a1509d48d239b71f9a1
MaketendoDev <najim.oria59@hotmail.com>
2024-07-06 23:42:50 +0100

    weak dry stone plank fix

--- src/main/java/net/maketendo/tardifmod/procedures/WeakPetrifiedPlanksEntityWalksOnTheBlockProcedure.java
+++ src/main/java/net/maketendo/tardifmod/procedures/WeakPetrifiedPlanksEntityWalksOnTheBlockProcedure.java
@@ -10,24 +10,20 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.BlockPos;
 
-import net.maketendo.tardifmod.TardifModMod;
-
 public class WeakPetrifiedPlanksEntityWalksOnTheBlockProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
 		boolean beingbroken = false;
 		if (beingbroken == false) {
-			TardifModMod.queueServerWork(40, () -> {
-				world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
-				world.levelEvent(2001, BlockPos.containing(x, y, z), Block.getId(Blocks.OAK_PLANKS.defaultBlockState()));
-				if (world instanceof Level _level) {
-					if (!_level.isClientSide()) {
-						_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.zombie.break_wooden_door")), SoundSource.BLOCKS, 1, 1);
-					} else {
-						_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.zombie.break_wooden_door")), SoundSource.BLOCKS, 1, 1, false);
-					}
-				}
-			});
 			beingbroken = true;
+			world.setBlock(BlockPos.containing(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+			world.levelEvent(2001, BlockPos.containing(x, y, z), Block.getId(Blocks.OAK_PLANKS.defaultBlockState()));
+			if (world instanceof Level _level) {
+				if (!_level.isClientSide()) {
+					_level.playSound(null, BlockPos.containing(x, y, z), ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.zombie.break_wooden_door")), SoundSource.BLOCKS, 1, 1);
+				} else {
+					_level.playLocalSound(x, y, z, ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity.zombie.break_wooden_door")), SoundSource.BLOCKS, 1, 1, false);
+				}
+			}
 		}
 	}
 }
